da0a6e5b4a3de779383446cf0fbdf17a293bdb69
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-27 20:04:23 -0700

    Middleware creatd for JSON and urlencoded data

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,8 @@ const api = require('./routes/index.js');
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
