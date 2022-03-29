f46f5236878d43ebb0e17e22269aeb4e0677899f
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-28 19:09:43 -0700

    POST route added to notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -8,3 +8,23 @@ const { v4: uuidv4 } = require('uuid');
 note.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+// POST Route for a notes
+note.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
