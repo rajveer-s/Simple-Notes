44c5b2cb9dc27fb89e6f933ab609b0a78229a61c
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-28 19:09:20 -0700

    GET route added to notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -3,3 +3,8 @@ const note = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+
+// GET Route for retrieving all the notes
+note.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
